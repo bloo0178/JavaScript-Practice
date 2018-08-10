5e723809fea72ef6aef876dd08d7a4b56ad1e27e
bloo0178 <40503652+bloo0178@users.noreply.github.com>
2018-08-10 11:24:58 -0400

    Update IAS_Fibonacci.js

--- IAS_Fibonacci.js
+++ IAS_Fibonacci.js
@@ -1,5 +1,7 @@
 console.clear();
 
+//THIS IS A TEST
+
 /*Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
 
 The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
